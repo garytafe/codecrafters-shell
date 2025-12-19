c34fc1227408f0d1123f9959187461756e1122c0
garytafe <readimedic@tallstreet.com>
2025-12-19 05:08:37 +0000

    Add echo

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,16 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();    
-        if command.trim() == "exit" {
+        io::stdin().read_line(&mut command).unwrap();   
+        let text: Vec<&str> = command.split_whitespace().collect(); 
+        let program = text[0];
+        if program == "exit" {
             break;
+        }
+        if program == "echo" {
+            let output = &command[5..]; 
+            println!("{}", output.trim());
+            continue;
         }
         println!("{}: command not found", command.trim())
     }
