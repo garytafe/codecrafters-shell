47d1a00a577a754935466e5e875f18755fbeafd6
garytafe <readimedic@tallstreet.com>
2025-12-19 04:58:12 +0000

    Add repl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();    
-    println!("{}: command not found", command.trim())
-
+    while true {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();    
+        println!("{}: command not found", command.trim())
+    }
 }
